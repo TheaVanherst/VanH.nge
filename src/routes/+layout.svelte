41c1bd75f7b1ad70efff84e0706fa85dab843b19
Thea <TheaVanherst@gmail.com>
2023-01-11 19:43:24 +0000

    Test commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,7 @@
 			<div class="col1 col">
 				test lol xd test lol xdtest lol xdtest lol xdtest lol xd
 				test lol xd test lol xdtest lol xdtest lol xdtest lol xd
+				test lol xd test lol xdtest lol xdtest lol xdtest lol xd
 			</div>
 			<div class="col2 col">
 				<slot/>
