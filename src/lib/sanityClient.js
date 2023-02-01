84472a85b5d5dc78f553f59c0e490fe8047c65a0
Thea <TheaVanherst@gmail.com>
2023-02-01 20:59:24 +0000

    Environment Var fix
    Took 1 minute

--- src/lib/sanityClient.js
+++ src/lib/sanityClient.js
@@ -2,8 +2,8 @@
 import sanityClient from "@sanity/client"
 
 const client = sanityClient({
-    projectId: import.meta.env.SANITY_PROJECT_ID,
-    dataset: import.meta.env.SANITY_DATASET,
+    projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+    dataset: import.meta.env.VITE_SANITY_DATASET,
     token: '', // or leave blank for unauthenticated usage
     useCdn: true,
 });
