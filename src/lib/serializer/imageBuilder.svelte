924902061584cda9dccdba211e3951b956053283
Thea <TheaVanherst@gmail.com>
2023-03-06 16:21:23 +0000

    Added aspect ratio calculator for the image builder
    Took 19 seconds

--- src/lib/serializer/imageBuilder.svelte
+++ src/lib/serializer/imageBuilder.svelte
@@ -4,34 +4,54 @@
 
     import client from "$lib/sanityClient.js";
     export let image = null;
-    let imageRef = undefined;
 
+    let loadCheck = undefined;
     let loaded = false;
+
     onMount(() => {
-        if (imageRef) {
-            imageRef.onload = () => {
+        if (loadCheck) {
+            loadCheck.onload = () => {
                 loaded = true;
             };
         }
     });
+
+    const ratioCalc = () => {
+        const lcd = (a, b) => { // finds the lowest common denominator
+            return (b === 0) ? a : lcd(b, a % b);
+        }
+
+        if (image?.asset){
+            let d = image.asset._ref;
+                d = d.split("-")[2];
+                d = d.split("x");
+
+            let w = d[0],
+                h = d[1];
+            let r = lcd(w, h);
+
+            return (w / r) + " / " + (h / r)
+        }
+    }
 
     const urlFor = (source) => {
         return imageUrlBuilder(client).image(source);
     };
 </script>
 
-{#if image.asset}
-    <div class:loaded
+{#if image?.asset}
+    <div style="aspect-ratio:{ratioCalc()}"
+         class:loaded
          class:nsfw={image?.NSFW}>
         <img loading="lazy"
-            src={ urlFor(image).width(1200) }
-            alt={ image?.alt }
-            bind:this={ imageRef }/>
+            src =   { urlFor(image).width(1200) }
+            alt =   { image?.alt }
+            bind:this={ loadCheck }/>
     </div>
 {/if}
 
 <style lang="scss">
-    $backgroundSize: 800px; // pretty sure I don't need this tbh.
+    $backgroundSize: 800px;
 
     div {
         height:     100%;
