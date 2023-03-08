8dbe3e1d3885e4398f3ecd0a6b4ee1465f930eca
Thea <TheaVanherst@gmail.com>
2023-03-08 22:40:38 +0000

    Fixed scroll gallery effecting overflow on parent containers
    Took 17 minutes

--- src/lib/serializer/types/galleryTypes/scroll.svelte
+++ src/lib/serializer/types/galleryTypes/scroll.svelte
@@ -15,43 +15,14 @@
 
 <style lang="scss">
 	.table {
-		position:   relative;
+        display:        grid;
+        overflow-y:     hidden;
+        white-space:    nowrap;
 
-		scrollbar-width:            thin;
-		-webkit-overflow-scrolling: touch;
-
-		scroll-snap-align:  start;
-		scroll-snap-type:   x mandatory;
-		scroll-snap-stop:   always;
-		scroll-behavior:    smooth;
-
-		&::-webkit-scrollbar {
-			height: 		var(--innerRaidus);}
-		&::-webkit-scrollbar-track {
-			background: 	var(--backgroundAccent2);}
-		&::-webkit-scrollbar-thumb {
-			border-radius: 	2px;
-			border: 		1px solid var(--background);
-			background: 	var(--accent1);}
-		&::-webkit-scrollbar-thumb:hover {
-			background: 	var(--darkAccent1);}
-
-		.row {
-            gap:        var(--imageSpacing);
-            display:    block;
-            overflow:   scroll;
-            position:   relative;
-
-            white-space: nowrap;
-
-	        .col {
-		        background-color:   var(--backgroundAccent2);
-
-                display: inline-block;
-		        overflow:   hidden;
-		        height:     400px;
-		        width:      100%;
-            }
+        .col {
+            background: var(--backgroundAccent2);
+            display:    inline-grid;
+            height:     100%;
         }
     }
 </style>
